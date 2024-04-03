f8b6f729fded13058c0a2acfd6e6e9a9c371db54
Cyprien Ferreira <cyferrei@bess-f1r2s10.clusters.42paris.fr>
2024-04-03 17:16:51 +0200

    [ADD]: add includes

---
+++ includes/pipex.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 15:53:15 by cyferrei          #+#    #+#             */
+/*   Updated: 2024/04/03 16:21:36 by cyferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+/*all includes*/
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+
+/*main structure*/
+
+typedef struct s_pipe
+{
+	pid_t	pid_1;
+	pid_t	pid_2;
+	int tube[2];
+	int	infile;
+	int outfile;
+	char	*paths;
+	char	*cmd;
+	char	**paths_cmd;
+	char	**args_cmd;
+}			t_pipe;
+
+/*enum garbage for error*/
+
+enum	e_state
+{
+	ERROR_INPUT = 1,
+};
+
+/*all prototyopes*/
+
+void	exit_error(enum e_state error);
+
+#endif
