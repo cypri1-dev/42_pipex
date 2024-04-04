47830fd8f82372338f23d3012c17c7e56d759be1
Cyprien Ferreira <cyferrei@bess-f1r3s9.clusters.42paris.fr>
2024-04-04 11:56:44 +0200

    [ADD]: add exit_error function and continue parsing

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 15:52:57 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/04/03 16:22:29 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/04/03 17:42:43 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,10 @@ int	main(int argc, char **argv, char **envp)
 	if (argc != 5)
 		exit_error(ERROR_INPUT);
 	return (0);
+	pipe.infile = open(argv[1], O_RDONLY);
+	if (pipe.infile < 0)
+		exit_error(ERROR_INFILE);
+	pipe.outfile = open(argv[argc -1], O_TRUNC | O_CREAT | O_RDWR, 0644);
+	if (pipe.outfile < 0)
+		exit_error(ERROR_OUTFILE);
 }
--- sources/pipex_utils.c
+++ sources/pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 16:12:20 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/04/03 16:24:18 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/04/03 17:32:06 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ void	exit_error(enum e_state error)
 	if(error == ERROR_INPUT)
 	{
 		write(2, "Not enought arguments!\n", 26);
+		return;
+	} 
+	else if(error = ERROR_INFILE)
+	{
+		write(2, "Error with infile!\n", 19);
 		return;
 	}
 }
