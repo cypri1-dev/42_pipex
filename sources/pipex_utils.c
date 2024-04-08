6bc6c4f723f21656af6b8796fe0c0be1481bdcc4
Cyprien Ferreira <cyferrei@bess-f1r4s12.clusters.42paris.fr>
2024-04-08 16:53:36 +0200

    [ADD]: add free functions

--- sources/pipex_utils.c
+++ sources/pipex_utils.c
@@ -6,11 +6,39 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 16:12:20 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/04/04 16:24:13 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/04/08 15:51:12 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
+
+void 	free_parent_process(t_pipe *my_pipe)
+{
+	int i;
+
+	i = 0;
+	close(my_pipe->infile);
+	close(my_pipe->outfile);
+	while(my_pipe->paths_cmd[i])
+	{
+		free(my_pipe->paths_cmd[i]);
+		i++;
+	}
+	free(my_pipe->paths_cmd);
+}
+
+void	free_child_process(t_pipe *my_pipe)
+{
+	int	i;
+
+	i = 0;
+	while(my_pipe->args_cmd[i])
+	{
+		free(my_pipe->args_cmd[i]);
+		i++;
+	}
+	free(my_pipe->args_cmd);
+}
 
 char	*get_path(char **envp)
 {
