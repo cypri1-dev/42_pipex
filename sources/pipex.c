051f9f8dacd55c7cb69ddc79bf60bf75ea4993d7
Cyprien Ferreira <cyferrei@bess-f1r4s12.clusters.42paris.fr>
2024-04-08 16:59:49 +0200

    [ADD]: add final implementation - free management - waitpid

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 15:52:57 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/04/04 19:57:40 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/04/08 16:28:35 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,26 +46,32 @@ int	main(int argc, char **argv, char **envp)
 	my_pipe.infile = open(argv[1], O_RDONLY);
 	if (my_pipe.infile < 0)
 		exit_error("Error\nError with intfile !\n");
-	my_pipe.outfile = open(argv[argc -1], O_TRUNC | O_CREAT | O_RDWR, 0644);
+	my_pipe.outfile = open(argv[argc -1], O_TRUNC | O_CREAT | O_WRONLY, 0644);
 	//printf("d = %d\n", my_pipe.outfile);
 	if (my_pipe.outfile < 0)
 	{
-		close(my_pipe.infile);
+		//close(my_pipe.infile);
 		exit_error("Error\nWError with outfile !\n");
 	}
-	my_pipe.paths = get_path(envp);
-	my_pipe.paths_cmd = ft_split(my_pipe.paths, ':');
 	if (pipe(my_pipe.tube) < 0)
 		exit_error("Error\nPipe failed !\n");
+	my_pipe.paths = get_path(envp);
+	my_pipe.paths_cmd = ft_split(my_pipe.paths, ':');
 	my_pipe.pid_1 = fork();
+	//printf("%d\n", my_pipe.pid_1);
 	if (my_pipe.pid_1 == 0)
 		first_child_process(my_pipe, argv, envp);
-	//printf("path --> %s\n", my_pipe.paths);
-	//dipslay_cmd(my_pipe.paths_cmd);
-	close(my_pipe.infile);
-	close(my_pipe.outfile);
+	// printf("path --> %s\n", my_pipe.paths);
+	// dipslay_cmd(my_pipe.paths_cmd);
+	my_pipe.pid_2 = fork();
+	if (my_pipe.pid_2 == 0)
+		second_child_process(my_pipe, argv, envp);
+	close(my_pipe.tube[0]);
+	close(my_pipe.tube[1]);
+	waitpid(my_pipe.pid_1, NULL, 0);
+	waitpid(my_pipe.pid_2, NULL, 0);
 	free(my_pipe.paths);
-	free_cmd(&my_pipe);
+	free_parent_process(&my_pipe);
 	return (0);
 }
 
