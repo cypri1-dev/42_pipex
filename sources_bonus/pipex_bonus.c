bd44d55ea92f51521f4282e83e33646d1ed2a6c8
Cyprien Ferreira <cyferrei@bess-f1r4s12.clusters.42paris.fr>
2024-04-11 14:08:26 +0200

    [ADD]: add begining for bonus part

---
+++ sources_bonus/pipex_bonus.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 11:27:58 by cyferrei          #+#    #+#             */
+/*   Updated: 2024/04/11 13:01:34 by cyferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex_bonus.h"
+
+static int	parse_args(char *first_arg, t_pipeb *pipe)
+{
+	if(first_arg && ft_strncmp("here_doc", first_arg, 9) == 0)
+	{
+		pipe->here_doc = 1;
+		return (SIX_ARGS_INPUT);
+	}
+	else
+	{
+		pipe->here_doc = 0;
+		return (FIVE_ARGS_INPUT);	
+	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_pipeb pipe;
+	
+	(void)envp;
+	if(argc < parse_args(argv[1], &pipe))
+		exit_error("Error\nInvalid args detected!\n");
+	printf("here_doc --> %d (1-yes | 0-no)\n", pipe.here_doc);
+	return(0);
+}
