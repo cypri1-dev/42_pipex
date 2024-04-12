6bb5605364d7505be08b2018b3221d82ee3d1f9a
Cyprien Ferreira <cyferrei@bess-f1r3s2.clusters.42paris.fr>
2024-04-12 21:28:27 +0200

    [MODIF]: modif on mandotory part - some fds were still open - PATCH

--- sources/children_cmd.c
+++ sources/children_cmd.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 16:57:00 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/04/08 17:22:51 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/04/12 17:18:21 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,11 @@ static char	*make_cmd(char **paths, char *cmd)
 void	first_child_process(t_pipe my_pipe, char **argv, char **envp)
 {
 	dup2(my_pipe.infile, STDIN_FILENO);
+	close(my_pipe.infile);
+	close(my_pipe.outfile);
 	close(my_pipe.tube[0]);
 	dup2(my_pipe.tube[1], STDOUT_FILENO);
+	close(my_pipe.tube[1]);
 	my_pipe.args_cmd = ft_split(argv[2], ' ');
 	my_pipe.cmd = make_cmd(my_pipe.paths_cmd, my_pipe.args_cmd[0]);
 	if (!my_pipe.cmd)
@@ -52,8 +55,11 @@ void	first_child_process(t_pipe my_pipe, char **argv, char **envp)
 void	second_child_process(t_pipe my_pipe, char **argv, char **envp)
 {
 	dup2(my_pipe.tube[0], STDIN_FILENO);
+	close(my_pipe.tube[0]);
 	close(my_pipe.tube[1]);
 	dup2(my_pipe.outfile, STDOUT_FILENO);
+	close(my_pipe.outfile);
+	close(my_pipe.infile);
 	my_pipe.args_cmd = ft_split(argv[3], ' ');
 	my_pipe.cmd = make_cmd(my_pipe.paths_cmd, my_pipe.args_cmd[0]);
 	if (!my_pipe.cmd)
